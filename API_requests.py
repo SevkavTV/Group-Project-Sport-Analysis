fe823d32dc8510887d743c052ec80c456f966ef9
SevkavTV <seva.archakov@gmail.com>
2021-05-16 18:52:28 +0300

    flask structure

--- API_requests.py
+++ API_requests.py
@@ -5,6 +5,13 @@ class Requests:
     APIkey = '3f561cce229d17fa05f23e5fa9f1ce750c39652d1aa8fed1ae58464da66706a4'
 
     @staticmethod
+    def get_available_leagues():
+        request_url = f'https://apiv2.apifootball.com/?action=get_leagues&APIkey={Requests.APIkey}'
+        response = requests.get(request_url)
+
+        return response.json()
+
+    @staticmethod
     def get_teams_by_league_id(league_id):
         request_url = f'https://apiv2.apifootball.com/?action=get_teams&league_id={league_id}&APIkey={Requests.APIkey}'
         response = requests.get(request_url)
--- app.py
+++ app.py
@@ -5,110 +5,34 @@ Main file to run the server
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from analysis import Analysis
+from API_requests import Requests
 
 # initialize Flask
 app = Flask(__name__)
 CORS(app)
-db = Database(DatabaseConfig)
 
 
-@app.route('/api/get_all_entries', methods=['POST'])
-def get_all_entries():
+@app.route('/api/get_available_leagues', methods=['GET'])
+def get_available_leagues():
     '''
-    Get all books with response as info about them including given column.
+    Get all available leagues.
     '''
-    request_data = request.json
+    available_leagues = Requests.get_available_leagues()
 
-    if 'column_names' not in request_data.keys():
-        err = 'No column names key in request'
-        return make_response(jsonify(error=err), 400)
-
-    cols = request_data['column_names']
-
-    not_found_columns = [col for col in cols if col not in db.data.columns]
-
-    if len(not_found_columns) > 0:
-        err = f"Columns {not_found_columns} are not in our database"
-        return make_response(jsonify(error=err), 400)
-
-    all_books = db.get_all_books(cols)
-
-    return make_response(jsonify(all_books), 200)
+    return make_response(jsonify(available_leagues), 200)
 
 
-@app.route('/api/get_entries_by_param', methods=['POST'])
-def get_entries_by_param():
+@app.route('/api/get_teams_by_league_id', methods=['POST'])
+def get_teams_by_league_id():
     '''
-    Get all books which have match with the given param in their info.
+    Get all teams in league by its id.
     '''
     request_data = request.json
 
-    if 'param' not in request_data.keys():
-        err = 'No parameter in request'
-        return make_response(jsonify(error=err), 400)
+    league_id = request_data['league_id']
+    teams = Requests.get_teams_by_league_id(league_id)
 
-    param = request_data['param']
-
-    if isinstance(param, str):
-        found_books = db.find_books_by_param(param)
-
-        return make_response(jsonify(found_books), 200)
-
-    err = "Param type is not string"
-    return make_response(jsonify(error=err), 400)
-
-
-@app.route('/api/get_book_info_by_ISBN', methods=['POST'])
-def get_book_info_by_ISBN():
-    '''
-    Get all info about book by its ISBN value.
-    '''
-    request_data = request.json
-
-    if 'ISBN' not in request_data.keys():
-        err = 'No ISBN in request'
-        return make_response(jsonify(error=err), 400)
-
-    ISBN = request_data['ISBN']
-
-    if isinstance(ISBN, str):
-        book_info = db.get_book_info(ISBN)
-
-        return make_response(jsonify(book_info), 200)
-
-    err = "Param ISBN is not string"
-    return make_response(jsonify(error=err), 400)
-
-
-@app.route('/api/filter_books', methods=['POST'])
-def filter_books():
-    '''
-    Filter books with the given parameters.
-    '''
-    request_data = request.json
-
-    if 'operator' not in request_data.keys():
-        err = 'No operator in request, allowed values are AND/OR'
-        return make_response(jsonify(error=err), 400)
-
-    if request_data['operator'] not in ['AND', 'OR']:
-        err = 'No such operator is supported'
-        return make_response(jsonify(error=err), 400)
-
-    operator = request_data['operator']
-
-    not_found_columns = [col for col in request_data.keys() if (
-        col not in db.data.columns and col != 'operator')]
-
-    if len(not_found_columns) > 0:
-        err = f"Columns {not_found_columns} are not in our database"
-        return make_response(jsonify(error=err), 400)
-
-    filter_dict = {key: value for key,
-                   value in request_data.items() if key in db.data.columns}
-    filtered_books = db.filter_books(filter_dict, operator=operator)
-
-    return make_response(jsonify(filtered_books), 200)
+    return make_response(jsonify(teams), 200)
 
 
 if __name__ == '__main__':
